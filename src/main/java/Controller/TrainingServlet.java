5d68a5f3fdf2e45d66e6c979ccd9bb03a1b07b75
GagliardeNicolapio <gagliardenicolapio00@gmail.com>
2022-01-18 16:42:10 +0100

    Aggiunta metodo scrittura indici, aggiunta rimozione righe con campi vuoti

--- src/main/java/Controller/TrainingServlet.java
+++ src/main/java/Controller/TrainingServlet.java
@@ -7,7 +7,11 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 
@@ -38,18 +42,12 @@ public class TrainingServlet extends Controller {
             ConverterUtils.DataSource dataSource = new ConverterUtils.DataSource("C:\\Users\\gagli\\Desktop\\FakeAndTrueRandomWithCovidTest.arff");
             Instances instances = dataSource.getDataSet();
 
-            //setto il num di colonna della var target
-            if(instances.classIndex() == -1)
-                instances.setClassIndex(instances.numAttributes()-1);
-
             //DATA CLEANING
-            //VEDERE COME CANCELLARE LE RIGHE CON CAMPI VUOTI
-            //riempio i campi vuoti //instances.attributeStats(0).missingCount per vedere i campi vuoti //ok funziona
-            System.out.println("Campi vuoti colonna text: "+instances.attributeStats(1).missingCount);
-            ReplaceMissingWithUserConstant replaceMissingValues = new ReplaceMissingWithUserConstant();
-            replaceMissingValues.setInputFormat(instances);//forse bisogna rimettere ReplaceMissingValues
-            instances = Filter.useFilter(instances,replaceMissingValues);//quale constante scegliamo?//se non scegliamo niente mette 0
-            System.out.println("Campi vuoti colonna text: "+instances.attributeStats(1).missingCount);
+            System.out.println("Num campi vuoti: "+instances.attributeStats(1).missingCount);
+            System.out.println("Num attributes: "+instances.numInstances());
+            instances.removeIf(Instance::hasMissingValue);
+            System.out.println("Num campi vuoti: "+instances.attributeStats(1).missingCount);
+            System.out.println("Num attributes: "+instances.numInstances());
 
 
             //FEATURE SELECTION
@@ -59,12 +57,13 @@ public class TrainingServlet extends Controller {
             remove.setInputFormat(instances);
             instances = Filter.useFilter(instances,remove);*/
 
+            //STRING TO WORD VECTOR
             System.out.println("Inizio to word vector");
             StringToWordVector stringToWordVector = new StringToWordVector();
             stringToWordVector.setIDFTransform(true);
             stringToWordVector.setTFTransform(true);
             stringToWordVector.setAttributeIndices("first-last"); //tutti gli indici
-            stringToWordVector.setStemmer(new LovinsStemmer());
+            stringToWordVector.setStemmer(new SnowballStemmer());
             stringToWordVector.setStopwordsHandler(new Rainbow());
             WordTokenizer wordTokenizer = new WordTokenizer();
             wordTokenizer.setDelimiters(".,;:'\"()?!/ -_><&#");
@@ -73,35 +72,55 @@ public class TrainingServlet extends Controller {
             stringToWordVector.setWordsToKeep(1000000);
             instances = Filter.useFilter(instances,stringToWordVector);
             System.out.println("fine to word vector");
-            System.out.println(stringToWordVector.getWordsToKeep());
 
-            System.out.println("---"+instances.numAttributes());
-            System.out.println(instances.numInstances());
-            System.out.println(instances.numClasses());
+            //setto il num di colonna della var target
+            if(instances.classIndex() == -1)
+                instances.setClassIndex(instances.numAttributes()-1);
 
-            NaiveBayes naiveBayes = new NaiveBayes();
-            Evaluation evaluation = new Evaluation(instances);
-            evaluation.crossValidateModel(naiveBayes, instances, K_FOLDS, new Random(new Date().getTime()));
-            System.out.println("Naive Bayes: "+evaluation.toClassDetailsString());
+            System.out.println("Num attributes: "+instances.numAttributes());
+            System.out.println("Num instances: "+instances.numInstances());
+            System.out.println("Num classes: "+instances.numClasses());
 
+            //ATTRIBUTE SELECTION
             System.out.println("inizio selection");
             AttributeSelection attributeSelection = new AttributeSelection();
             CfsSubsetEval eval = new CfsSubsetEval();
             BestFirst bestFirst = new BestFirst();
             attributeSelection.setSearch(bestFirst);
             attributeSelection.setEvaluator(eval);
-
             attributeSelection.SelectAttributes(instances);
             int[] indices = attributeSelection.selectedAttributes();
-            System.out.println(Utils.arrayToString(indices));
-            System.out.println(indices.length);
+            System.out.println("Indici da conservare: "+Utils.arrayToString(indices));
+            System.out.println("Num tot indici: "+indices.length);
+            writeIndices(indices); //salvo gli indici in un file
 
+            //ATTRIBUTE REMOVE
+            int j=0;
+            for(int i=0; i<instances.numAttributes(); i++){
+                if(indices[j]!=i){
+                    instances.remove(i);
+                }else{
+                    j++;
+                    continue;
+                }
+            }
+
+            System.out.println("Num tot attibutes rimasti: "+instances.numAttributes());
+
+
+            //ADDESTRAMENTO NAIVE BAYES
+            NaiveBayes naiveBayes = new NaiveBayes();
+            Evaluation evaluation = new Evaluation(instances);
+            evaluation.crossValidateModel(naiveBayes, instances, K_FOLDS, new Random(new Date().getTime()));
+            System.out.println("Naive Bayes: "+evaluation.toClassDetailsString());
+
+            //ADDESTRAMENTO J48
             J48 decisionTree = new J48();
             Evaluation evaluationTree = new Evaluation(instances);
             evaluationTree.crossValidateModel(decisionTree, instances, K_FOLDS, new Random(new Date().getTime()));
             System.out.println("Decision Tree: "+ evaluationTree.toClassDetailsString());
 
-
+            //SALVATAGGIO MODELLI
             SerializationHelper.write("../model/naiveBayes.model", naiveBayes);
             SerializationHelper.write("../model/j48.model", decisionTree);
             getServletContext().setAttribute("naiveModel",SerializationHelper.read("../model/naiveBayes.model"));
@@ -113,9 +132,19 @@ public class TrainingServlet extends Controller {
     }
 
 
+    public void writeIndices(int[] indices) throws IOException {
+        FileWriter fileWriter = new FileWriter("C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\dataset\\indici.txt");
+        for(int i:indices)
+            fileWriter.append(i+"\n");
+
+        fileWriter.append("\n"+"totale: "+indices.length);
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 }
