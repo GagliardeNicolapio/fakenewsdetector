bc0e338044a1abc2832b252deff92187c502eac9
GagliardeNicolapio <gagliardenicolapio00@gmail.com>
2022-01-18 15:39:23 +0100

    Aggiunta della ricerca delle parole piu correlate

--- src/main/java/Controller/TrainingServlet.java
+++ src/main/java/Controller/TrainingServlet.java
@@ -11,11 +11,14 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Random;
 
+import weka.attributeSelection.*;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.J48;
 import weka.core.*;
 import weka.core.converters.ConverterUtils;
+import weka.core.stemmers.LovinsStemmer;
 import weka.core.stemmers.SnowballStemmer;
 import weka.core.stopwords.Rainbow;
 import weka.core.tokenizers.WordTokenizer;
@@ -40,10 +43,13 @@ public class TrainingServlet extends Controller {
                 instances.setClassIndex(instances.numAttributes()-1);
 
             //DATA CLEANING
+            //VEDERE COME CANCELLARE LE RIGHE CON CAMPI VUOTI
             //riempio i campi vuoti //instances.attributeStats(0).missingCount per vedere i campi vuoti //ok funziona
+            System.out.println("Campi vuoti colonna text: "+instances.attributeStats(1).missingCount);
             ReplaceMissingWithUserConstant replaceMissingValues = new ReplaceMissingWithUserConstant();
-            replaceMissingValues.setInputFormat(instances);
-            instances = Filter.useFilter(instances,replaceMissingValues);
+            replaceMissingValues.setInputFormat(instances);//forse bisogna rimettere ReplaceMissingValues
+            instances = Filter.useFilter(instances,replaceMissingValues);//quale constante scegliamo?//se non scegliamo niente mette 0
+            System.out.println("Campi vuoti colonna text: "+instances.attributeStats(1).missingCount);
 
 
             //FEATURE SELECTION
@@ -58,19 +64,37 @@ public class TrainingServlet extends Controller {
             stringToWordVector.setIDFTransform(true);
             stringToWordVector.setTFTransform(true);
             stringToWordVector.setAttributeIndices("first-last"); //tutti gli indici
-            stringToWordVector.setStemmer(new SnowballStemmer());
+            stringToWordVector.setStemmer(new LovinsStemmer());
             stringToWordVector.setStopwordsHandler(new Rainbow());
-            stringToWordVector.setTokenizer(new WordTokenizer());
+            WordTokenizer wordTokenizer = new WordTokenizer();
+            wordTokenizer.setDelimiters(".,;:'\"()?!/ -_><&#");
+            stringToWordVector.setTokenizer(wordTokenizer);
             stringToWordVector.setInputFormat(instances);
+            stringToWordVector.setWordsToKeep(1000000);
             instances = Filter.useFilter(instances,stringToWordVector);
             System.out.println("fine to word vector");
+            System.out.println(stringToWordVector.getWordsToKeep());
 
+            System.out.println("---"+instances.numAttributes());
+            System.out.println(instances.numInstances());
+            System.out.println(instances.numClasses());
 
             NaiveBayes naiveBayes = new NaiveBayes();
             Evaluation evaluation = new Evaluation(instances);
             evaluation.crossValidateModel(naiveBayes, instances, K_FOLDS, new Random(new Date().getTime()));
             System.out.println("Naive Bayes: "+evaluation.toClassDetailsString());
 
+            System.out.println("inizio selection");
+            AttributeSelection attributeSelection = new AttributeSelection();
+            CfsSubsetEval eval = new CfsSubsetEval();
+            BestFirst bestFirst = new BestFirst();
+            attributeSelection.setSearch(bestFirst);
+            attributeSelection.setEvaluator(eval);
+
+            attributeSelection.SelectAttributes(instances);
+            int[] indices = attributeSelection.selectedAttributes();
+            System.out.println(Utils.arrayToString(indices));
+            System.out.println(indices.length);
 
             J48 decisionTree = new J48();
             Evaluation evaluationTree = new Evaluation(instances);
@@ -82,6 +106,7 @@ public class TrainingServlet extends Controller {
             SerializationHelper.write("../model/j48.model", decisionTree);
             getServletContext().setAttribute("naiveModel",SerializationHelper.read("../model/naiveBayes.model"));
             getServletContext().setAttribute("dTreeModel",SerializationHelper.read("../model/j48.model"));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
